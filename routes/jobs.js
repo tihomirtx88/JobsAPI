13b416d1ae8cfcfc203d6ed58297e053a4f0805f
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-30 09:13:45 +0200

    Getting single job by user id functionality

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,14 +4,14 @@ const router = express.Router()
 const {
   createJob,
 //   deleteJob,
-//   getAllJobs,
+ getAllJobs,
 //   updateJob,
-//   getJob,
+getJob,
 //TODO 
 } = require('./../controllers/jobs');
 
-router.route('/').post(createJob);
+router.route('/').post(createJob).get(getAllJobs)
 
-// router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+router.route('/:id').get(getJob);
 
 module.exports = router;
