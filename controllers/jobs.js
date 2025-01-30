f213d85dd77105feaf7af2f5c365b301b7e6f5b5
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-30 08:26:05 +0200

    Implement get all jobs functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,12 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 
+
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
+
 const createJob = async (req, res) => {
     try {
   
