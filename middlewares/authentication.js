d8d00cd72db648a420f717f61712188965c11886
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-30 08:17:38 +0200

    Refacturiing code and complited logic for creating job

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,23 +3,29 @@ const jwt = require("jsonwebtoken");
 const { UnauthenticatedError } = require("./../errors");
 
 const auth = async (req, res, next) => {
-   //check header
    const authHeader = req.headers.authorization;
-   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new UnauthenticatedError('Authentication not valid');
+   if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new UnauthenticatedError('Authentication invalid')
    }
-
-   const token = authHeader.split(' ')[1];
-
+ 
+   const token = authHeader.split(" ")[1];
+ 
    try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Attach uset to the job router
-    req.user = {userId: payload.userId, name:payload.name};
-    next();
-
+     const payload = jwt.verify(token, process.env.JWT_SECRET);
+     
+     // Fetch user from DB
+     const user = await User.findById(payload.userId).select("-password");
+     
+     if (!user) {
+       return res.status(401).json({ error: "User not found" });
+     }
+ 
+     req.user = { userId: user._id, name: user.name };
+ 
+ 
+     next();
    } catch (error) {
-      throw new UnauthenticatedError('Authentication not valid');
+      throw new UnauthenticatedError('Authentication invalid')
    }
 };
 module.exports = auth;
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,16 +2,16 @@ const express = require('express');
 
 const router = express.Router()
 const {
-//   createJob,
+  createJob,
 //   deleteJob,
 //   getAllJobs,
 //   updateJob,
 //   getJob,
 //TODO 
-} = require('../controllers/jobs');
+} = require('./../controllers/jobs');
 
-router.route('/').post(createJob).get(getAllJobs);
+router.route('/').post(createJob);
 
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+// router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
 
 module.exports = router;
