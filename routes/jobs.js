c15b90899bb229cab00faf0c9617c15e0e6ae85f
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-31 07:30:42 +0200

    Implement delete job fuinctionality

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 const {
   createJob,
-//   deleteJob,
- getAllJobs,
- updateJob,
-getJob,
-//TODO 
-} = require('./../controllers/jobs');
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+} = require("./../controllers/jobs");
 
-router.route('/').post(createJob).get(getAllJobs)
+router.route("/").post(createJob).get(getAllJobs);
 
-router.route('/:id').get(getJob).patch(updateJob);
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
 
 module.exports = router;
