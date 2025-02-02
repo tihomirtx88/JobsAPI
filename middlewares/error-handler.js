a089db3dde4774a2cca1595fc5ccbaac9746af87
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-02 11:39:21 +0200

    Implemenet cast error lgoic

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -27,6 +27,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
 
+  if (err.name === 'CastError') {
+    customError.message = `Bot item find with id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  // Cast  error
+
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.message })
 }
