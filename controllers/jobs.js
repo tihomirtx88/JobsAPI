b2ed00ced5b079c37e74136c91875ac873740cab
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-05 18:44:37 +0200

    Implement monthly agregration pipline functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -141,8 +141,8 @@ const deleteJob = async (req, res) => {
   }
 }
 
-const showStats = (req, res) => {
-  let stats = Job.aggregate([
+const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
     {$match: {createdBy: mongoose.Types.ObjectId(req.user.userId)}},
     {$group: {_id: '$status', count: {$sum: 1}}}
   ]);
@@ -159,7 +159,24 @@ const showStats = (req, res) => {
     declined: stats.declined || 0
   };
 
-   res.status(StatusCodes.OK).json({defaultStats, monthlyApplications: []});
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications.map((item) => {
+    const {_id: { year, month }, count} = item;
+    const date = moment().month(month - 1).year(year).format('MMM Y');
+    return {date, count};
+  });
+   res.status(StatusCodes.OK).json({defaultStats, monthlyApplications});
 };
 
 
