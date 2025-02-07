7fdd28778f7960634193d57431e21da96029fae3
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-07 21:45:30 +0200

    Implement error handling in image upload functionality

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,15 +1,34 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const CustomError = require("../errors");
 
 const uploadUserImage = async (req, res) => {
-    console.log(req);
-    
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+
   const userImage = req.files.image;
-  const imagePath = path.join(__dirname, '../public/uploads' + `${userImage.name}`);
+
+  if (!userImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please upload image!');
+  }
+
+  const maxSize = 1024 * 1024;
+  if (userImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller then 1kb');
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads" + `${userImage.name}`
+  );
+
   await userImage.mv(imagePath);
-  return res.status(StatusCodes.OK).json({image: {src: `/uploads/${userImage.name}`}});
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${userImage.name}` } });
 };
 
 module.exports = {
-    uploadUserImage
+  uploadUserImage,
 };
