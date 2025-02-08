f4d99b34af50199b25eff9c1aae0aba7461a11c4
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-08 16:12:23 +0200

    impolement functionality to upload images in cloudinary platform

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,6 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
 const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 const uploadUserImage = async (req, res) => {
   if (!req.files) {
@@ -29,6 +31,17 @@ const uploadUserImage = async (req, res) => {
     .json({ image: { src: `/uploads/${userImage.name}` } });
 };
 
+const uploadImageWithCloudynary = async (req, res) => {
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+        use_filename: true,
+        folder: 'upload_images_jobs_API'
+    });
+    
+    fs.unlinkSync(req.files.image.tempFilePath);
+    return res.status(StatusCodes.OK).json({image: {src: result.secure_url}});
+};
+
 module.exports = {
   uploadUserImage,
+  uploadImageWithCloudynary
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, updateUser } = require('../controllers/auth');
-const { uploadUserImage } = require("../controllers/uploadsController");
+const { uploadUserImage, uploadImageWithCloudynary } = require("../controllers/uploadsController");
 const authenticateUser = require("./../middlewares/authentication");
 const testUser = require("./../middlewares/testUser");
 
 router.post('/register', register);
 router.post('/login', login);
 router.patch('/updateUser',authenticateUser, testUser, updateUser);
-router.route('/uploads').post(uploadUserImage);
+router.route('/uploads').post(uploadImageWithCloudynary);
 
 module.exports = router;
